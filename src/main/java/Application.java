16b2546153791ce01643ceae4ed9b0e28b79d86e
Izabela Gajewska <iza.gajewska93@gmail.com>
2020-07-21 09:09:29 +0200

    colors,

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,10 +1,9 @@
 public class Application {
     public static void main(String[] args) {
 
-        Person adam = new Person();
-        adam.name = "Adam";
-        adam.age = 40.5;
-        adam.height = 178;
+        Person adam = new Person("Adam", 40.5, 178);
+        Person iza = new Person("Iza",26, 168);
+        System.out.println(iza.name + " " + iza.age + " " + iza.height);
 
         if (adam.name != null) {
             if (adam.age > 30 && adam.height > 160) {
